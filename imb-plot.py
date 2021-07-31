c0d9d98a69df4226abd96e5c43c8bef56db09b27
Atsushi Hori <ahori@riken.jp>
2021-07-31 22:44:46 +0900

    Fix bugs

--- imb-plot.py
+++ imb-plot.py
@@ -20,8 +20,8 @@ def my_readline( f ):
 
     if len( ch_str ) > 0:
         ch_str = ch_str[0:-1]   # remove newline
-        tokens = ch_str.split()
-        return tokens
+    tokens = ch_str.split()
+    return tokens
 
 def get_benchmark( f ):
     while( True ):
@@ -32,11 +32,15 @@ def get_benchmark( f ):
         if nc > 3:
             if tokens[1] == 'BAD' and tokens[2] == 'TERMINATION':
                 print( 'BAD TERMINATION' )
-                sys.exit( 1 )
+                return None
+            continue
         if nc != 3:
             continue
         if tokens[1] == 'Benchmarking':
             bench = tokens[2]
+            if not bench in benchmarks:
+                print( 'Unsupported benchmark: ', bench )
+                continue
             break
 
     tokens = my_readline( f )
@@ -49,8 +53,12 @@ def get_benchmark( f ):
         if tokens is None:
             return None
         if len( tokens ) == 1 and tokens[0][0] == '#' and tokens[0][1] == '-':
-            my_readline( f )    # skip header
-            return ( bench, nprocs )
+            while( True ) :
+                tokens = my_readline( f )
+                if tokens[0] == '#bytes' or tokens[0] == '#repetitions':
+                    break;
+            break
+    return ( bench, nprocs )
 
 def ping( f, bench, np ):
     df = pd.DataFrame( [], columns=['Latency','Bandwidth'] )
@@ -58,7 +66,10 @@ def ping( f, bench, np ):
         tokens = my_readline( f )
         if tokens == [] or tokens is None:
             break;
-        df.loc[ int(tokens[0]) ] = [ float(tokens[2]), float(tokens[3]) ]
+        try:
+            df.loc[ int(tokens[0]) ] = [ float(tokens[2]), float(tokens[3]) ]
+        except:
+            break
 
     df.to_csv( fbase+bench+'.csv', sep=',' )
 
@@ -67,15 +78,19 @@ def ping( f, bench, np ):
     ax0.set_xscale('log')
     ax0.set_yscale('log')
     ax0.grid()
+    ax0.set_ylabel( 'Latency' )
+    ax0.set_xlabel( 'Message Size [Bytes]' )
     ax1 = ax0.twinx()
     ax1.grid( axis='y', linestyle='dotted' )
     ax1.set_yscale('log')
+    ax1.set_ylabel( 'Bandwidth' )
+
     ax0.plot( df['Latency'],   label='Latency',   color='Red'  )
     ax1.plot( df['Bandwidth'], label='Bandwidth', color='Blue' )
     ( h0, l0 ) = ax0.get_legend_handles_labels()
     ( h1, l1 ) = ax1.get_legend_handles_labels()
     ax1.legend( h0+h1, l0+l1, loc='upper left' )
-    plt.title( bench + comment )
+    plt.title( bench )
     plt.savefig( fbase+bench+'.pdf' )
     plt.close( 'all' )
 
@@ -85,17 +100,22 @@ def exchange( f, bench, np ):
     df_lat = pd.DataFrame( [], columns=[ np ] )
     df_bdw = pd.DataFrame( [], columns=[ np ] )
     while( True ):
-        tokens = my_readline( f )
-        if tokens == [] or tokens is None:
-            break;
-        df_lat.loc[ int(tokens[0]), np ] = float(tokens[4])
-        df_bdw.loc[ int(tokens[0]), np ] = float(tokens[5])
+        while( True ):
+            tokens = my_readline( f )
+            if tokens == [] or tokens is None:
+                break;
+            try:
+                df_lat.loc[ int(tokens[0]), np ] = float(tokens[4])
+                df_bdw.loc[ int(tokens[0]), np ] = float(tokens[5])
+            except:
+                break
         rv = get_benchmark( f )
         if rv is None or rv[0] != bench:
             break
+        np = rv[1]
 
     df_lat.to_csv( fbase+bench+'-latency.csv',   sep=',' )
-    ax = df_lat.plot( title=bench+' (latency)'+comment,
+    ax = df_lat.plot( title=bench+' (latency)',
                       loglog=True, grid=True, legend='reverse' )
     ax.set_xlabel( 'Length (bytes)' )
     ax.set_ylabel( 'Latency (usec)' )
@@ -103,9 +123,9 @@ def exchange( f, bench, np ):
     plt.close( 'all' )
 
     df_bdw.to_csv( fbase+bench+'-bandwidth.csv', sep=',' )
-    ax = df_bdw.plot( title=bench+' (bandwidth)'+comment,
+    ax = df_bdw.plot( title=bench+' (bandwidth)',
                       loglog=True, grid=True )
-    ax.set_xlabel( 'Length (bytes)' )
+    ax.set_xlabel( 'Message Size [Bytes]' )
     ax.set_ylabel( 'Bandwidth (Mbytes/sec)' )
     plt.savefig( fbase+bench+'-bandwidth.pdf' )
     plt.close( 'all' )
@@ -115,19 +135,24 @@ def exchange( f, bench, np ):
 def collective( f, bench, np ):
     df = pd.DataFrame( [], columns=[ np ] )
     while( True ):
-        tokens = my_readline( f )
-        if tokens == [] or tokens is None:
-            break;
-        df.loc[ int(tokens[0]), np ] = float(tokens[4])
+        while( True ):
+            tokens = my_readline( f )
+            if tokens == [] or tokens is None:
+                break;
+            try:
+                df.loc[ int(tokens[0]), np ] = float(tokens[4])
+            except:
+                break
         rv = get_benchmark( f )
         if rv is None or rv[0] != bench:
             break
+        np = rv[1]
 
     df.to_csv( fbase+bench+'.csv', sep=',' )
 
-    ax = df.plot( title=bench+comment,
+    ax = df.plot( title=bench,
                   loglog=True, grid=True, legend='reverse' )
-    ax.set_xlabel( 'Length (bytes)' )
+    ax.set_xlabel( 'Message Size [Bytes]' )
     ax.set_ylabel( 'Latency (usec)' )
     plt.savefig( fbase+bench+'.pdf' )
     plt.close( 'all' )
@@ -137,45 +162,62 @@ def collective( f, bench, np ):
 def barrier( f, bench, np ):
     df = pd.DataFrame( [], columns=[ 'Latency' ] )
     while( True ):
-        tokens = my_readline( f )
-        if tokens == [] or tokens is None:
-            break;
-        df.loc[ np ] = float(tokens[3])
+        while( True ):
+            tokens = my_readline( f )
+            if tokens == [] or tokens is None:
+                break;
+            try:
+                df.loc[ np ] = float(tokens[3])
+            except:
+                break
         rv = get_benchmark( f )
         if rv is None or rv[0] != bench:
             break
+        np = rv[1]
 
     df.to_csv( fbase+bench+'.csv', sep=',' )
     
-    ax = df.plot( title=bench+comment,
+    ax = df.plot( title=bench,
                   grid=True, marker='*', ylim=(0,None), legend=None )
     ax.set_xscale( 'log' )
-    ax.set_xlabel( '# procs' )
-    ax.set_ylabel( 'Latency (usec)' )
+    ax.set_xlabel( '# Procs' )
+    ax.set_ylabel( 'Latency [usec]' )
     plt.savefig( fbase+bench+'.pdf' )
     plt.close( 'all' )
 
     return rv
 
-benchmarks = { 'PingPong': 		ping,
-               'PingPing': 		ping,
-               'Sendrecv': 		exchange,
-               'Exchange': 		exchange,
-               'Allreduce': 		collective,
-               'Reduce': 		collective,
-               'Reduce_local':		collective,
-               'Reduce_scatter': 	collective,
-               'Reduce_scatter_block': 	collective,
-               'Allgather':		collective,
-               'Allgatherv':		collective,
-               'Gather':		collective,
-               'Gatherv':		collective,
-               'Scatter':		collective,
-               'Scatterv':		collective,
-               'Alltoall':		collective,
-               'Alltoallv':		collective,
-               'Bcast':			collective,
-               'Barrier':		barrier }
+benchmarks = {
+    # MPI1
+    'PingPong': 		ping,
+    'PingPing': 		ping,
+    'Sendrecv': 		exchange,
+    'Exchange': 		exchange,
+    'Allreduce': 		collective,
+    'Reduce': 			collective,
+    'Reduce_local':		collective,
+    'Reduce_scatter': 		collective,
+    'Reduce_scatter_block': 	collective,
+    'Allgather':		collective,
+    'Allgatherv':		collective,
+    'Gather':			collective,
+    'Gatherv':			collective,
+    'Scatter':			collective,
+    'Scatterv':			collective,
+    'Alltoall':			collective,
+    'Alltoallv':		collective,
+    'Bcast':			collective,
+    'Barrier':			barrier,
+    # RMA
+    'Put_all_local':		ping,
+    'One_put_all':		ping,
+    'All_put_all':		collective,
+    'One_get_all':		ping,
+    'All_get_all':		collective,
+    'Exchange_put':		collective,
+    'Exchange_get':		collective,
+    'Accumulate':		ping
+}
 
 argc = len( sys.argv )
 if argc == 0:
@@ -183,22 +225,15 @@ if argc == 0:
     sys.exit( 1 )
 
 for i in range( 1, argc ):
-    imb_outfile = sys.argv[i]
-    fbase = os.path.splitext( os.path.basename( imb_outfile ) )[0] + '-'
-    if argc > 2:
-        comment = ' ' + sys.argv[2]
-    else:
-        comment = ''
+    imb_datfile = sys.argv[i]
+    print( imb_datfile )
+    fbase = os.path.splitext( os.path.basename( imb_datfile ) )[0] + '-'
 
-    with open( imb_outfile, mode='r' ) as f:
+    with open( imb_datfile, mode='r' ) as f:
         rv = get_benchmark( f )
         while rv != None:
             ( bench, nproc ) = rv
-            if not bench in benchmarks:
-                print( 'Unknown benchmark: ', bench )
-                ( bench, nproc ) = get_benchmark( f )
-            else:
-                print( bench )
-                rv = benchmarks[bench]( f, bench, nproc )
+            print( '   ', bench )
+            rv = benchmarks[bench]( f, bench, nproc )
 
 sys.exit( 0 )
